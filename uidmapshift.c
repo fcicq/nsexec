19577201090cc341642a1213b0d26a886546ca85
Serge Hallyn <serge.hallyn@canonical.com>
2013-02-25 14:58:14 -0600

    apply patch from Dwight Engen to greatly improve uidmapshift.

--- uidmapshift.c
+++ uidmapshift.c
@@ -1,160 +1,149 @@
+#define _XOPEN_SOURCE 500
+#include <errno.h>
+#include <ftw.h>
+#include <getopt.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-void usage(char *me)
+#define min(a,b) (a) < (b) ? (a) : (b)
+#define max(a,b) (a) > (b) ? (a) : (b)
+
+static int verbose = 0;
+static int convert_uids = 0;
+static int convert_gids = 0;
+static uid_t srcid;
+static uid_t dstid;
+static uid_t range;
+static uid_t range_uid_max = 0;
+static uid_t range_uid_min = ~0;
+static gid_t range_gid_max = 0;
+static gid_t range_gid_min = ~0;
+
+void usage(void)
 {
-	printf("Usage: %s directory u nsuid hostuid range\n", me);
-	printf("       %s directory g nsgid hostgid range\n", me);
-	printf("       %s directory b nsid  hostid  range\n", me);
-	printf("       Shifts all ids under directory\n");
-	printf("       'b' means shift both uids and gids to the same mapping\n");
-	exit(1);
+	extern const char *__progname;
+	printf("Usage: %s [OPTIONS] directory [src dst range]\n\n", __progname);
+	printf("  -u, --uid <src dst range>      convert uids in directory\n");
+	printf("  -g, --gid <src dst range>      convert gids in directory\n");
+	printf("  -b, --both <src dst range>     convert uids and gids in directory\n");
+	printf("  -r, --range                    find min,max uid/gid used in directory\n");
+	printf("  -v, --verbose                  increate verbosity\n\n");
+	printf("Note this program always recursively walks all of directory.\n");
+	printf("The -u,-g, and -b options require a [src dst range], to convert the\n");
+	printf("ids within the range [src..src+range] to [dst..dst+range].\n\n");
+	printf("Examples:\n");
+	printf("  %s -r /path/to/directory                # show min/max uid/gid\n", __progname);
+	printf("  %s -b /path/to/directory 0 100000 500   # map uids and gids up\n", __progname);
+	printf("  %s -u /path/to/directory 100000 0 500   # map the uids back down\n", __progname);
 }
 
-int convert_uids = 0, convert_gids = 0;
-
-//#define DEBUG 1
-
-int convert(char *base, int nsuid, int hostid, int r)
+int ftw_callback(const char *fpath, const struct stat *st,
+		 int typeflag, struct FTW *ftw)
 {
+	uid_t new_uid = -1;
+	uid_t new_gid = -1;
 	int ret;
-	struct stat mystat;
-	uid_t u;
-	gid_t g;
-	int converted = 0;
 
-	ret = lstat(base, &mystat);
-	if (ret)
-		return -1;
-	u = mystat.st_uid;
-	g = mystat.st_gid;
+	range_uid_max = max(range_uid_max, st->st_uid);
+	range_uid_min = min(range_uid_min, st->st_uid);
+	range_gid_max = max(range_gid_max, st->st_gid);
+	range_gid_min = min(range_gid_min, st->st_gid);
 
-#if DEBUG
-	fprintf(stderr, "mode on %s is %o\n", base, mystat.st_mode);
-#endif
-	if (convert_uids && u >= nsuid && u < nsuid+r) {
-		ret = lchown(base, (u-nsuid) + hostid, -1);
+	if (convert_uids && st->st_uid >= srcid && st->st_uid < srcid+range)
+		new_uid = (st->st_uid-srcid) + dstid;
+	if (convert_gids && st->st_gid >= srcid && st->st_gid < srcid+range)
+		new_gid = (st->st_gid-srcid) + dstid;
+	if (new_uid != -1 || new_gid != -1) {
+		ret = lchown(&fpath[ftw->base], new_uid, new_gid);
 		if (ret) {
-			fprintf(stderr, "failed to chown %s\n", base);
+			fprintf(stderr, "failed to chown %d:%d %s\n",
+				new_uid, new_gid, fpath);
 			/* well, let's keep going */
-		}
-		converted = 1;
-	}
-	if (convert_gids && g >= nsuid && g < g+r) {
-		ret = lchown(base, -1, (g-nsuid) + hostid);
-		if (ret) {
-			fprintf(stderr, "failed to chgrp %s\n", base);
-			/* well, let's keep going */
-		}
-		converted = 1;
-	}
-
-	if (converted && !S_ISLNK(mystat.st_mode)) {
-#if DEBUG
-		fprintf(stderr, "resetting mode on %s to %o\n", base, mystat.st_mode);
-#endif
-		ret = chmod(base, mystat.st_mode);
-		if (ret) {
-			fprintf(stderr, "failed to reset st_mode on %s\n", base);
-			/* well, let's keep going */
-		}
-	}
-	return 0;
-}
-
-#define GUESS 256
-int recursive_convert(char *base, int nsuid, int hostid, int r)
-{
-	char *pathname;
-	struct dirent dirent, *direntp;
-	DIR *dir;
-	int ret;
-	int len;
-
-	dir = opendir(base);
-	if (!dir) {
-		fprintf(stderr, "failed to open %s\n", base);
-		return -1;
-	}
-
-	len = strlen(base) + GUESS;
-	pathname = malloc(len);
-	if (!pathname)
-		return -1;
-
-	while (!readdir_r(dir, &dirent, &direntp)) {
-		struct stat mystat;
-		char *p;
-		uid_t u;
-		gid_t g;
-		int converted;
-
-		if (!direntp)
-			break;
-
-		if (!strcmp(direntp->d_name, ".") ||
-				!strcmp(direntp->d_name, ".."))
-			continue;
-
-		if (strlen(base) + strlen(direntp->d_name)+2 > len) {
-			len = strlen(base) + strlen(direntp->d_name) + 2;
-			p = realloc(pathname, len);
-			if (!p) {
-				free(pathname);
-				return -1;
+		} else {
+			if (!S_ISLNK(st->st_mode)) {
+				if (verbose > 1)
+					fprintf(stderr, "resetting mode to %o on %s\n",
+						st->st_mode, fpath);
+				ret = chmod(&fpath[ftw->base], st->st_mode);
+				if (ret) {
+					fprintf(stderr, "failed to reset mode %o on %s\n",
+						st->st_mode, fpath);
+					/* well, let's keep going */
+				}
 			}
-			pathname = p;
+			if (verbose)
+				printf("u:%07d=%07d g:%07d=%07d m:%#07o %s %s\n",
+				       st->st_uid, new_uid,
+				       st->st_gid, new_gid,
+				       st->st_mode, fpath, &fpath[ftw->base]);
 		}
-
-		ret = snprintf(pathname, len, "%s/%s", base, direntp->d_name);
-		if (ret < 0 || ret >= len) {
-			free(pathname);
-			return -1;
-		}
-		ret = lstat(pathname, &mystat);
-		if (ret)
-			continue;
-		if (S_ISDIR(mystat.st_mode) && !S_ISLNK(mystat.st_mode))
-			recursive_convert(pathname, nsuid, hostid, r);
-		convert(pathname, nsuid, hostid, r);
 	}
-
-	if (pathname)
-		free(pathname);
-	closedir(dir);
 	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	char *base;
-	int nsuid,  // uid to use in namespace
-	    hostid,  // uid to map to on host
-	    range;
-	int ret;
+	const char *base;
+	int show_range = 0;
+	int opt,ret;
 
-	if (argc < 6 || strcmp(argv[1], "-h") == 0 ||
-			strcmp(argv[1], "--help") == 0)
-		usage(argv[0]);
+	static const struct option long_opts[] = {
+		{ "help",    no_argument, NULL, 'h' },
+		{ "uids",    no_argument, NULL, 'u' },
+		{ "gids",    no_argument, NULL, 'g' },
+		{ "both",    no_argument, NULL, 'b' },
+		{ "range",   no_argument, NULL, 'r' },
+		{ "verbose", no_argument, NULL, 'v' },
+		{ NULL,      0,           NULL, 0   }
+	};
 
-	base = argv[1];
-	switch (*argv[2]) {
-	case 'u': convert_uids = 1; break;
-	case 'g': convert_gids = 1; break;
-	case 'b': convert_uids = convert_gids = 1; break;
-	default: usage(argv[0]);
+        while ((opt = getopt_long(argc, argv, "hugbrv", long_opts, NULL)) >= 0) {
+		switch (opt) {
+		case 'h': usage(); exit(EXIT_SUCCESS);
+		case 'u': convert_uids = 1; break;
+		case 'g': convert_gids = 1; break;
+		case 'b': convert_uids = convert_gids = 1; break;
+		case 'r': show_range = 1; break;
+		case 'v': verbose++; break;
+		}
 	}
-	nsuid = atoi(argv[3]);
-	hostid = atoi(argv[4]);
-	range = atoi(argv[5]);
 
-	ret = recursive_convert(base, nsuid, hostid, range);
-	if (ret)
-		return ret;
-	return convert(base, nsuid, hostid, range);
+	argc -= optind;
+	argv += optind;
+
+	if (argc < 1) {
+		usage();
+		exit(EXIT_FAILURE);
+	}
+
+	base = argv[0];
+	if (convert_uids || convert_gids) {
+		if (argc < 4) {
+			usage();
+			exit(EXIT_FAILURE);
+		}
+		srcid = atoi(argv[1]);
+		dstid = atoi(argv[2]);
+		range = atoi(argv[3]);
+	}
+
+	ret = nftw(base, ftw_callback, 1000, FTW_PHYS|FTW_CHDIR);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to walk path %s %s\n", base, strerror(errno));
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	if (show_range) {
+		printf("UIDs %d - %d\n"
+		       "GIDs %d - %d\n",
+		       range_uid_min, range_uid_max,
+		       range_gid_min, range_gid_max);
+	}
+
+	return EXIT_SUCCESS;
 }
